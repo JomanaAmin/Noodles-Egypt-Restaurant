c6614b9f1f8c58c49cb4eced0dda64eab56dd06a
JOMANA HELLO <j.hussein2327@nu.edu.eg>
2026-07-14 13:41:57 +0300

    Created DbContext, added it to  Program.cs, added first migration

---
+++ WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NoodlesEgypt.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        // Define your DbSets (tables) here
+        //public DbSet<YourEntity> YourEntities { get; set; }
+    }
+}
