f0e5968b61c90d11f5de2fb3017fc88487b8233b
JOMANA HELLO <j.hussein2327@nu.edu.eg>
2026-07-14 16:34:42 +0300

    Implemented Generic repository

--- WebApplication1/Data/Repositories/IBaseRepository.cs
+++ WebApplication1/Data/Repositories/IBaseRepository.cs
@@ -1,6 +1,12 @@
 ﻿namespace WebApplication1.Data.Repositories
 {
-    public interface IBaseRepository
+    public interface IBaseRepository<T> where T : class
     {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        public void Update(T entity);
+        Task<T?> DeleteAsync(int id);
+        IQueryable<T> GetAllAsQueryable();
     }
 }
