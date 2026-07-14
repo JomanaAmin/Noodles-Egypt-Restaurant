a1b3a3bfab0c319550fdddbd49630321447ab096
JOMANA HELLO <j.hussein2327@nu.edu.eg>
2026-07-14 16:50:26 +0300

    Added UnitOfWork

---
+++ WebApplication1/Data/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using NoodlesEgypt.Data;
+using WebApplication1.Data.Entities;
+using WebApplication1.Data.Repositories;
+
+namespace WebApplication1.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext dbContext;
+        private Lazy<IBaseRepository<Product>> products;
+        private Lazy<IBaseRepository<ProductVariant>> productVariants;
+        private Lazy<IBaseRepository<Category>> categories;
+
+        public UnitOfWork(AppDbContext dbContext) 
+        {
+            this.dbContext = dbContext;
+            products = new Lazy<IBaseRepository<Product>>(() => new BaseRepository<Product>(dbContext));
+            productVariants = new Lazy<IBaseRepository<ProductVariant>>(() => new BaseRepository<ProductVariant>(dbContext));
+            categories = new Lazy<IBaseRepository<Category>>(() => new BaseRepository<Category>(dbContext));
+        }
+        public IBaseRepository<Product> Products => products.Value;
+
+        public IBaseRepository<ProductVariant> ProductVariants => productVariants.Value;
+
+        public IBaseRepository<Category> Categories => categories.Value;
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await dbContext.SaveChangesAsync();
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using NoodlesEgypt.Data;
 using Microsoft.AspNetCore.Identity;
+using WebApplication1.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
 builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
 var app = builder.Build();
 
